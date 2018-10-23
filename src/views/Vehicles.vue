1cc4b7b0f8d215a6ff41e9a53584ea2f5ac0fcf3
jashment <jordanashment@gmail.com>
2018-10-23 15:24:53 -0600

    Created Vehicles.vue

--- src/views/Vehicles.vue
+++ src/views/Vehicles.vue
@@ -2,20 +2,20 @@
     <div>
         <v-container grid-list-xl>
           <v-layout row wrap>
-            <v-flex v-for="starships in allStarships" :key="`1${i}`" xs4>
+            <v-flex v-for="vehicles in allVehicles" :key="`1${i}`" xs4>
               <v-card>
                 <v-card-title primary-title>
                   <div>
-                    <h3 class="headline mb-0">{{starships.name}}</h3>
+                    <h3 class="headline mb-0">{{vehicles.name}}</h3>
                     <hr>
                     <v-list-group>
-                      <div>Model: {{starships.model}}</div>
-                      <span>Manufacturer: {{starships.manufacturer}}</span>
-                      <div>Cost in Credits: {{starships.cost_in_credits}}</div>
-                      <div>Length: {{starships.length}}</div>
-                      <div>Max Atmosphering Speed: {{starships.max_atmosphering_speed}}</div>
-                      <div>Crew: {{starships.crew}}</div>
-                      <div>Starship Class: {{starships.starship_class}}</div>
+                      <div>Model: {{vehicles.model}}</div>
+                      <span>Manufacturer: {{vehicles.manufacturer}}</span>
+                      <div>Cost in Credits: {{vehicles.cost_in_credits}}</div>
+                      <div>Length: {{vehicles.length}}</div>
+                      <div>Max Atmosphering Speed: {{vehicles.max_atmosphering_speed}}</div>
+                      <div>Crew: {{vehicles.crew}}</div>
+                      <div>Vehicle Class: {{vehicles.vehicle_class}}</div>
                     </v-list-group>
                   </div>
                 </v-card-title>
@@ -47,7 +47,7 @@ export default {
         console.log(people);
     },
     mounted() {
-    axios.get("https://swapi.co/api/starships")
+    axios.get("https://swapi.co/api/vehicles")
     .then(response => {this.info = response.data})
   }
 }
