0152d70df7c01ad713be0bf52f72c39aad064a71
jashment <jordanashment@gmail.com>
2018-10-24 15:42:06 -0600

    Creating Login Form

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,21 +3,43 @@
         <v-layout>
             <v-flex xs6 class="ma-auto">
                 <v-card>
+                    <div>
                     <v-form>
-                        <p>Email Goes Here</p>
+                        <v-text-field v-model="name" :rules="nameRules" :counter="10" label="Name" required></v-text-field>
+                        <v-spacer></v-spacer>
+                        <v-text-field v-model="email" :rules="emailRules" label="E-mail" required></v-text-field>
+                        <v-spacer></v-spacer>
+                        <v-text-field v-model="password" label="Password" :type="show1 ? 'text' : 'password'"></v-text-field>
+                        <v-btn @click="clear">clear</v-btn>
                     </v-form>
+                    </div>
                 </v-card>
             </v-flex>
+            <p>{{email}}</p>
+            <p>{{password}}</p>
         </v-layout>
     </v-container>
 </template>
 
 <script>
 export default {
-    
+    data () {
+        return {
+            show1: false,
+            password: 'Password',
+            email: 'Email'
+        }
+    },
+    methods: {
+        clear () {
+            this.$refs.form.reset();
+        }
+    }
 }
 </script>
 
 <style scoped>
-
+    div {
+        padding: .5em;
+    }
 </style>
