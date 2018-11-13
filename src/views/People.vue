85b0abd09f17832fb51dc460e5b4ac3ec012133b
jashment <jordanashment@gmail.com>
2018-11-12 19:01:53 -0700

    Getting frustrated with axios and SWAPI

--- src/views/People.vue
+++ src/views/People.vue
@@ -2,10 +2,11 @@
     <div>
         <v-container grid-list-xl>
           <v-layout row wrap>
-            <v-flex v-for="people in allPeople" :key="`1${i}`" xs4>
+            <v-flex v-for="people in SWArray" :key="`1${i}`" xs4>
               <v-card>
                 <v-card-title primary-title>
                   <div>
+                    
                     <h3 class="headline mb-0">{{people.name}}</h3>
                     <hr>
                     <v-list-group class="animated fadeInDown">
@@ -26,69 +27,27 @@
             </v-flex>
           </v-layout>
         </v-container>
-
-        <v-layout row>
-    <v-flex xs12 sm6 offset-sm3>
-      <v-card>
-
-
-        <v-list three-line>
-          <v-list-group>
-          <template v-for="people in allPeople">
-            <v-subheader
-              v-if="people.name"
-              :key="people.name"
-            >
-              {{ people.name }}
-            </v-subheader>
-
-            <v-subheader
-              v-if="people.gender"
-              :key="people.gender"
-            >
-              {{ people.gender }}
-            </v-subheader>
-
-            <v-divider
-              v-else-if="item.divider"
-              :inset="item.inset"
-              :key="people"
-            ></v-divider>
-
-            <v-list-tile
-              v-else
-              :key="people.height"
-              avatar
-              @click=""
-            >
-              <v-list-tile-avatar>
-                <img :src="item.avatar">
-              </v-list-tile-avatar>
-
-              <v-list-tile-content>
-                <v-list-tile-title v-html="people.name"></v-list-tile-title>
-                <v-list-tile-sub-title v-html="people.mass"></v-list-tile-sub-title>
-              </v-list-tile-content>
-            </v-list-tile>
-          </template>
-          </v-list-group>
-        </v-list>
-      </v-card>
-    </v-flex>
-  </v-layout>
     </div>
 </template>
 
 <script>
+import axios from 'axios'
 import {Mixins} from '../assets/Mixins.js';
 export default {
+  data () {
+    return {
+      SWArray: []
+    }
+  },
     mixins: [Mixins],
     created () {
         console.log(people);
     },
     mounted() {
-    axios.get("https://swapi.co/api/planets")
-    .then(response => {this.info = response.data})
+    axios
+        .get("/people")
+        .then(response => (this.SWArray = response.data.results))
+        console.log(SWArray)
   }
 }
 </script>
