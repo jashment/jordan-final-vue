51f40219d7341a1692f505b168ecb7a3629d51e8
jashment <jordanashment@gmail.com>
2019-04-24 23:26:20 -0600

    Updating About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,7 +3,7 @@
   <div>
     <br>
     <h1>About this Project</h1>
-    <p>This project uses the Vue.js Framework along with Vuetify, Axios, Vuex and Vuelidate to make a beautiful web app. This project uses Axios to bring information from the Swapi.co API and display it using the Vuetify component framework. See characters, planets, vehicles, species and starships from the Star Wars universe! </p>
+    <p>This project uses the Vue.js Framework along with Vuetify, Axios, Vuex and Vuelidate to make a beautiful web app. This project uses Axios to bring information from the Swapi.co API and display it using the Vuetify component framework. As database storage, Google's Firebase is used to store authentication information. See characters, planets, vehicles, species and starships from the Star Wars universe! </p>
   </div>
 </v-app>
 </template>
